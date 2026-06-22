7b98eeecbf555b387731a923b0c41e3890cead57
Alex <aaaaesvd@gmail.com>
2026-06-22 22:21:26 +0300

    feat: add  Header layout

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -1,7 +1,29 @@
+import classNames from 'classnames'
 import './Header.scss'
 import Logo from '@/components/Logo'
 
-const Header = () => {
+const Header = (props) => {
+  const {url} = props
+  const menuItems = [
+    {
+    label: "Home",
+    href: "/",
+    }, 
+    {
+    label: "Movies & Shows",
+    href: "/movies",
+    }, 
+    {
+    label: "Support",
+    href: "/support",
+    }, 
+    {
+    label: "Subscriptions",
+    href: "/subscriptions",
+    }, 
+  ] 
+
+
   return (
     <header className="header">
       <div className="header__inner container">
@@ -9,6 +31,22 @@ const Header = () => {
           className="header__logo"
           loading="eager"
         />
+        <nav className="header__menu">
+          <ul className="header__menu-list">
+            {menuItems.map(
+              ({label, href}, index) => (
+                <li className="header__menu-item" key={index}>
+                  <a 
+                    href={href}
+                    className={classNames("header__menu-link", {"is-active": href === url})}
+                  >
+                    {label}
+                  </a>
+                </li>
+              )
+            )}
+          </ul>
+        </nav>
       </div>
     </header>
   )
